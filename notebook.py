10cb9662e2ad10d3b5c2eec9ad9ff0646a5eba9b
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-06-04 13:47:24 -0400

    add initial requirements

---
+++ notebook.py
@@ -0,0 +1,22 @@
+"""Working notebook for DCIST Lab."""
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import spark_dsg as dsg
+
+# %%
+help(dsg)
